015eb88c03e016bef507a746a6dbd3f66e950e32
Cllaude99 <xodbsrla1025@naver.com>
2023-11-08 22:51:41 +0900

    feat:"Join"

--- frontend/doge/src/screens/Join.tsx
+++ frontend/doge/src/screens/Join.tsx
@@ -1,3 +1,30 @@
+import styled from "styled-components";
+
+const SubmitWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Submit = styled.div`
+  width: 80%;
+  height: 60px;
+  margin: 10px;
+  background-color: ${(props) => props.theme.orange};
+  border: 1px solid ${(props) => props.theme.orange};
+  border-radius: 10px;
+
+  font-size: 24px;
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${(props) => props.theme.white.lighter};
+    font-size: 24px;
+    padding-top: 13px;
+  }
+`;
+
 const Join = () => {
   return <h1>Join</h1>;
 };
