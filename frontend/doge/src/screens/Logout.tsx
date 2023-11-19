cb4b0e5a8110b7c0a9ba942ad8d904a6e2b83d80
Cllaude99 <xodbsrla1025@naver.com>
2023-11-19 16:54:27 +0900

    feat:"로그아웃 기능 구현"

--- frontend/doge/src/screens/Logout.tsx
+++ frontend/doge/src/screens/Logout.tsx
@@ -1,7 +1,8 @@
 import { useSetRecoilState } from "recoil";
 import { LoginState } from "../stores/atoms";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
+// 이부분을 컴포넌트로 만들 것인지 아니면, 헤더에서 로그인 svg 클릭했을때 함수를 통해 처리해줄 것인지 정할 필요있음
 const Logout = () => {
   const navigate = useNavigate();
   const setIsLogin = useSetRecoilState(LoginState);
