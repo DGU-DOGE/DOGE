138146e557782337cbf0834367152d18edd29ee6
Cllaude99 <xodbsrla1025@naver.com>
2023-11-19 11:47:45 +0900

    feat: "Login State 전역상태 관리"

--- frontend/doge/src/stores/atoms.tsx
+++ frontend/doge/src/stores/atoms.tsx
@@ -1,1 +1,13 @@
-import { atom, selector } from "recoil";
+import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist({
+  key: "localstorage", // this key is using to store data in local storage
+  storage: localStorage, // configure which storage will be used to store t
+});
+
+export const LoginState = atom<boolean>({
+  key: "LoginState",
+  default: false,
+  effects_UNSTABLE: [persistAtom],
+});
