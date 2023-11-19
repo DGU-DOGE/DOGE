efe20ef002ef0cb57733edbd3a6104696a1b6bdf
Cllaude99 <xodbsrla1025@naver.com>
2023-11-19 11:53:09 +0900

    LoginState에 따라 로그인 전역상태 수정

--- frontend/doge/src/screens/Logout.tsx
+++ frontend/doge/src/screens/Logout.tsx
@@ -1,4 +1,11 @@
+import { useSetRecoilState } from "recoil";
+import { LoginState } from "../stores/atoms";
+import { useNavigate } from "react-router-dom";
+
 const Logout = () => {
+  const navigate = useNavigate();
+  const setIsLogin = useSetRecoilState(LoginState);
+  setIsLogin(false);
   return <h1>Logout</h1>;
 };
 
