b5b117c513d7e63f0bef8bb4dcb97662467ee677
TaeYoon Kim <xodbsrla1025@naver.com>
2023-11-22 13:27:15 +0900

    feat:"Login 구현"

--- frontend/doge/src/screens/Login.tsx
+++ frontend/doge/src/screens/Login.tsx
@@ -31,7 +31,7 @@ const Title = styled.div`
   margin-top: 25px;
   h1 {
     font-size: 68px;
-    color: ${(props) => props.theme.orange};
+    color: ${props => props.theme.orange};
   }
 `;
 const LoginWrapper = styled.div`
@@ -48,8 +48,8 @@ const LoginForm = styled.form`
   }
   input[type="submit"] {
     cursor: pointer;
-    background-color: ${(props) => props.theme.orange};
-    color: ${(props) => props.theme.white.lighter};
+    background-color: ${props => props.theme.orange};
+    color: ${props => props.theme.white.lighter};
     font-size: 30px;
   }
 `;
@@ -57,8 +57,8 @@ const Input = styled.input`
   width: 80%;
   height: 60px;
   margin: 10px;
-  background-color: ${(props) => props.theme.gray.medium};
-  border: 1px solid ${(props) => props.theme.gray.medium};
+  background-color: ${props => props.theme.gray.medium};
+  border: 1px solid ${props => props.theme.gray.medium};
   border-radius: 10px;
   padding: 10px;
   font-size: 24px;
@@ -75,7 +75,7 @@ const Extra = styled.div`
   justify-content: flex-end;
   a,
   span {
-    color: ${(props) => props.theme.orange};
+    color: ${props => props.theme.orange};
     margin-left: 10px;
     font-size: 22px;
   }
@@ -84,7 +84,7 @@ const AlertMessage = styled.span`
   width: 80%;
   margin-left: 23px;
   margin-bottom: 10px;
-  color: ${(props) => props.theme.orange};
+  color: ${props => props.theme.orange};
   font-size: 20px;
 `;
 interface ILogin {
@@ -96,14 +96,14 @@ const Login = () => {
   const [isLogin, setIsLogin] = useRecoilState(LoginState);
   const navigate = useNavigate();
   const { mutate, isLoading, data } = useMutation(fetchLogin, {
-    onSuccess: (data) => {
+    onSuccess: data => {
       //로그인 성공 시 실행되는 부분
       // 서버에서 받은 토큰을 저장하고 로그인 상태를 전역적으로 관리하여야 함.
       //localStorage.setItem("accessToken", data.accessToken);
       // atom.tsx의 Login상태 변경 하는 코드필요 ,, setIsLogin(true);
       // 홈화면으로 이동하는 과정 navigate코드 쓸 필요있음  ,, navigate(`/`);
     },
-    onError: (error) => {
+    onError: error => {
       console.log(`로그인 실패2`, error);
     },
   });
