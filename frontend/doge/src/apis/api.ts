5892ce4dee43a6e564c7a919779b7ea8f576c94d
Cllaude99 <xodbsrla1025@naver.com>
2023-11-24 08:29:27 +0900

    test

--- frontend/doge/src/apis/api.ts
+++ frontend/doge/src/apis/api.ts
@@ -20,7 +20,7 @@ export interface IBook {
 }
 export const fetchLogin = async (userData: IUserData) => {
   const payload = { email: userData.userId, password: userData.userPassword };
-  const { data } = await axios.post(`/api/user/login`, payload, {
+  const { data } = await axios.post(`/user/login`, payload, {
     withCredentials: true,
   });
   return data;
