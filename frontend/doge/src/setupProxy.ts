3f3b7f6d70e6c32a9cf454acdc6146fa3adfb302
TaeYoon Kim <xodbsrla1025@naver.com>
2023-11-23 11:57:38 +0900

    test

--- frontend/doge/src/setupProxy.ts
+++ frontend/doge/src/setupProxy.ts
@@ -8,4 +8,11 @@ module.exports = function (app: any) {
       changeOrigin: true,
     })
   );
+  app.use(
+    "/user",
+    createProxyMiddleware({
+      target: "http://localhost:8080",
+      changeOrigin: true,
+    })
+  );
 };
