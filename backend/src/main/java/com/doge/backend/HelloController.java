0bfde9bf3d5cb9c3b1da5ec77db887f2fdae0f80
KiminSon <kimin0331@naver.com>
2023-11-24 15:09:29 +0900

    test: post 테스트

--- backend/src/main/java/com/doge/backend/HelloController.java
+++ backend/src/main/java/com/doge/backend/HelloController.java
@@ -1,6 +1,10 @@
 package com.doge.backend;
 
+import com.doge.backend.domain.member.Member;
+import com.doge.backend.domain.member.Role;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -10,7 +14,19 @@ import java.util.List;
 public class HelloController {
 
     @GetMapping("/api/demo-web")
-    public List<String> Hello(){
+    public List<String> Hello() {
         return Arrays.asList("리액트 스프링", "해치 웠나?");
+    }
+
+    @PostMapping("/test/doge")
+    public Member doge(@RequestBody Member member){
+        Member member1 = new Member();
+        member1.setId(1L);
+        member1.setEmail("backmail " + member.getEmail());
+        member1.setPassword("backpass " + member.getPassword());
+        member1.setFavoriteCount(0);
+        member1.setRole(Role.ROLE_USER);
+
+        return member1;
     }
 }
