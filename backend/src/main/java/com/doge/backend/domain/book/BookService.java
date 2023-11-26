e7196ba00eb3615b5fa81cc2cfc3e9e9d6de5b0d
KiminSon <kimin0331@naver.com>
2023-11-26 15:55:17 +0900

    feat: 책 검색할 때 null값도 리턴

--- backend/src/main/java/com/doge/backend/domain/book/BookService.java
+++ backend/src/main/java/com/doge/backend/domain/book/BookService.java
@@ -2,7 +2,6 @@ package com.doge.backend.domain.book;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -12,10 +11,6 @@ public class BookService {
     private final BookRepository bookRepository;
 
     List<Book> searchBook(String keyword) {
-        List<Book> books = bookRepository.findTop30ByBookNameContainsOrAuthorContains(keyword, keyword);
-        if (CollectionUtils.isEmpty(books)) {
-            throw new RuntimeException("없는 책");
-        }
-        return books;
+        return bookRepository.findTop30ByBookNameContainsOrAuthorContains(keyword, keyword);
     }
 }
