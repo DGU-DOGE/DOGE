37567afa82a8a16948df031f6fc6989a008d44b0
KiminSon <kimin0331@naver.com>
2023-11-26 10:06:39 +0900

    feat: 책, 저자 검색 구현

--- backend/src/main/java/com/doge/backend/domain/book/BookRepository.java
+++ backend/src/main/java/com/doge/backend/domain/book/BookRepository.java
@@ -2,5 +2,10 @@ package com.doge.backend.domain.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findTop30ByBookNameLike(String bookName);
+
+    List<Book> findTop30ByAuthorLike(String author);
 }
--- backend/src/main/java/com/doge/backend/domain/book/BookService.java
+++ backend/src/main/java/com/doge/backend/domain/book/BookService.java
@@ -3,7 +3,26 @@ package com.doge.backend.domain.book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BookService {
+    BookRepository bookRepository;
+
+    List<Book> searchBookName(String bookName) {
+        List<Book> books = bookRepository.findTop30ByBookNameLike(bookName);
+        if (books.isEmpty()) {
+            throw new RuntimeException("없는 책 이름");
+        }
+        return books;
+    }
+
+    List<Book> searchAuthor(String author) {
+        List<Book> books = bookRepository.findTop30ByAuthorLike(author);
+        if (books.isEmpty()) {
+            throw new RuntimeException("없는 저자");
+        }
+        return books;
+    }
 }
