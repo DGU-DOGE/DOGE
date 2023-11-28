e33c4b840428fb21a135ae5e40efb7e4b539f88c
KiminSon <kimin0331@naver.com>
2023-11-28 10:24:53 +0900

    refactor: 사용하지 않는 라이브러리 삭제

--- backend/src/main/java/com/doge/backend/domain/favorite/FavoriteService.java
+++ backend/src/main/java/com/doge/backend/domain/favorite/FavoriteService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
