3f7224419ecd7247c4211d19dfb430abd0a1251c
TaeYoon Kim <xodbsrla1025@naver.com>
2023-11-28 14:47:30 +0900

    test

--- frontend/doge/src/screens/UserInfo.tsx
+++ frontend/doge/src/screens/UserInfo.tsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import { ReactComponent as ElephantLogo } from "../assets/imgs/dgu-elephant.svg";
 import styled from "styled-components";
 import axios from "axios";
+import { useEffect, useState } from "react";
+import { getCookie } from "../stores/Cookie";
 
 const Wrapper = styled.div`
   min-width: 800px;
@@ -62,26 +64,31 @@ const Input = styled.input`
 
 const UserInfo = () => {
   const navigate = useNavigate();
-  const userEmail = "2019112056@dongguk.edu"; // 유저로 부터 받을 이메일 예시
-  // api.ts에서 fetchUserData로 부터 유저의 정보(유저 이메일, 즐겨찾기 목록 등)를 가져와서 화면에 보여주는 코드 필요
-  // 회원 정보 화면에는 즐겨찾기로 갈 수 있도록 하는 버튼과, 회원 탈퇴 버튼을 만들어 주고,
-  // 회원 탈퇴 버튼 클릭시 회원탈퇴 url로 유저의 아이디를 보낸다. (navigate의 state속성?)
-  // 회원 탈퇴 화면에서는 url로 부터 유저의 id를 얻고, 입력한 비밀번호를 백엔드에서 줄 회원 delete api로 보낸다.
-  // 해당 delete api요청이 성공적으로 이루어졌을 경우에는 atom.tsx에서 로그인된 상태를 false로 바꾸고, localStorage에 accessToken이 있을 경우, 해당 accessToken값을 제거해준다
+  const [userEmail, setUserEmail] = useState();
+  useEffect(() => {
+    (async () => {
+      axios
+        .post(`/api/user/check`, {
+          headers: {
+            sessionId: await getCookie("sessionId"),
+          },
+          withCredentials: true,
+        })
+        .then(res => {
+          console.log("사용자 email 받아오기 성공!");
+          setUserEmail(res.data);
+        })
+        .catch(err => console.log("사용자 email 받아오기 실패!"));
+    })();
+  }, []);
+
   const handleFavorite = () => {
     navigate("/favorites");
   };
-  const handleDeleteUser = (event: React.MouseEvent<HTMLInputElement>) => {
-    axios
-      .delete(``, { withCredentials: true }) //Authorization 필요
-      .then(() => {
-        //atoms.tsx참고해서 userLogin상태 false로 처리
-        //localStorage.removeItem("accessToken");
-        //alert("회원 탈퇴 완료");
-        navigate("/");
-      })
-      .catch(err => console.log("회원 탈퇴 실패", err));
+  const handleDeleteUser = () => {
+    navigate(`delete-account`);
   };
+
   return (
     <>
       <Wrapper>
@@ -101,10 +108,10 @@ const UserInfo = () => {
       <InfoWrapper>
         <Input
           value={userEmail}
-          readOnly
           onClick={event => {
             event.currentTarget.blur();
           }}
+          readOnly
         />
         <Input type="button" value={`즐겨찾기`} onClick={handleFavorite} />
         <Input type="button" value={`회원탈퇴`} onClick={handleDeleteUser} />
