19fc9c76ed2261dd83da93875aa152a67466a51a
TaeYoon Kim <xodbsrla1025@naver.com>
2023-12-05 14:24:36 +0900

    refactor:"Login refactor"

--- frontend/doge/src/screens/Login.tsx
+++ frontend/doge/src/screens/Login.tsx
@@ -7,6 +7,93 @@ import { LoginState } from "../stores/atoms";
 import axios from "axios";
 import { setCookie } from "../stores/Cookie";
 
+interface ILogin {
+  userId: string;
+  userPassword: string;
+}
+
+const Login = () => {
+  const [isLogin, setIsLogin] = useRecoilState(LoginState);
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILogin>({ mode: "onSubmit" });
+
+  const onValid = async (data: ILogin) => {
+    axios
+      .post(
+        `/api/user/login`,
+        { email: data.userId, password: data.userPassword },
+        { withCredentials: true }
+      )
+      .then(res => {
+        localStorage.setItem("sessionId", res.data.sessionId);
+        setCookie("sessionId", res.data.sessionId);
+        setIsLogin(true);
+        navigate(`/`);
+      })
+      .catch(err => {
+        console.log("로그인 실패", err);
+      });
+  };
+
+  return (
+    <Wrapper>
+      <Banner>
+        <BannerLogo>
+          <ElephantLogo />
+        </BannerLogo>
+        <Title>
+          <h1>로그인</h1>
+        </Title>
+      </Banner>
+      <LoginWrapper>
+        <LoginForm onSubmit={handleSubmit(onValid)}>
+          <Input
+            {...register("userId", {
+              required: "이메일을 입력해주세요",
+              pattern: {
+                value:
+                  /^(.+)@(dongguk\.edu|dgu\.edu|mail\.dgu\.edu|mail\.dongguk\.edu|dgu\.ac\.kr)$/,
+                message: "올바른 동국대학교 이메일 형식을 입력해주세요",
+              },
+            })}
+            placeholder="이메일을 입력하세요"
+          />
+          {errors.userId && errors.userId.type === "required" && (
+            <AlertMessage>{errors.userId.message}</AlertMessage>
+          )}
+          {errors.userId && errors.userId.type === "pattern" && (
+            <AlertMessage>{errors.userId.message}</AlertMessage>
+          )}
+          <Input
+            type="password"
+            {...register("userPassword", {
+              required: "비밀번호를 입력해주세요",
+            })}
+            placeholder="비밀번호를 입력하세요"
+          />
+          {errors.userPassword && errors.userPassword.type === "required" && (
+            <AlertMessage>{errors.userPassword.message}</AlertMessage>
+          )}
+          <Input type="submit" value="로그인" />
+        </LoginForm>
+      </LoginWrapper>
+      <ExtraWrapper>
+        <Extra>
+          <Link to={"/find-password"}>비밀번호 찾기</Link>
+          <span>/</span>
+          <Link to={"/join"}>회원가입</Link>
+        </Extra>
+      </ExtraWrapper>
+    </Wrapper>
+  );
+};
+
+export default Login;
+
 const Wrapper = styled.div`
   min-width: 800px;
   display: flex;
@@ -87,89 +174,3 @@ const AlertMessage = styled.span`
   color: ${props => props.theme.orange};
   font-size: 20px;
 `;
-interface ILogin {
-  userId: string;
-  userPassword: string;
-}
-
-const Login = () => {
-  const [isLogin, setIsLogin] = useRecoilState(LoginState);
-  const navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<ILogin>({ mode: "onSubmit" });
-
-  const onValid = async (data: ILogin) => {
-    axios
-      .post(
-        `/api/user/login`,
-        { email: data.userId, password: data.userPassword },
-        { withCredentials: true }
-      )
-      .then(res => {
-        console.log("로그인 성공!!!");
-        localStorage.setItem("sessionId", res.data.sessionId);
-        setCookie("sessionId", res.data.sessionId);
-        setIsLogin(true);
-        navigate(`/`);
-      })
-      .catch(err => {
-        console.log("로그인 실패", err);
-      });
-  };
-  return (
-    <Wrapper>
-      <Banner>
-        <BannerLogo>
-          <ElephantLogo />
-        </BannerLogo>
-        <Title>
-          <h1>로그인</h1>
-        </Title>
-      </Banner>
-      <LoginWrapper>
-        <LoginForm onSubmit={handleSubmit(onValid)}>
-          <Input
-            {...register("userId", {
-              required: "이메일을 입력해주세요",
-              pattern: {
-                value:
-                  /^(.+)@(dongguk\.edu|dgu\.edu|mail\.dgu\.edu|mail\.dongguk\.edu|dgu\.ac\.kr)$/,
-                message: "올바른 동국대학교 이메일 형식을 입력해주세요",
-              },
-            })}
-            placeholder="이메일을 입력하세요"
-          />
-          {errors.userId && errors.userId.type === "required" && (
-            <AlertMessage>{errors.userId.message}</AlertMessage>
-          )}
-          {errors.userId && errors.userId.type === "pattern" && (
-            <AlertMessage>{errors.userId.message}</AlertMessage>
-          )}
-          <Input
-            type="password"
-            {...register("userPassword", {
-              required: "비밀번호를 입력해주세요",
-            })}
-            placeholder="비밀번호를 입력하세요"
-          />
-          {errors.userPassword && errors.userPassword.type === "required" && (
-            <AlertMessage>{errors.userPassword.message}</AlertMessage>
-          )}
-          <Input type="submit" value="로그인" />
-        </LoginForm>
-      </LoginWrapper>
-      <ExtraWrapper>
-        <Extra>
-          <Link to={"/find-password"}>비밀번호 찾기</Link>
-          <span>/</span>
-          <Link to={"/join"}>회원가입</Link>
-        </Extra>
-      </ExtraWrapper>
-    </Wrapper>
-  );
-};
-
-export default Login;
