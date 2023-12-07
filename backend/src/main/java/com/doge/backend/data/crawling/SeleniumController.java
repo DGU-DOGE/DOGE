c1771d61ea16eadc7ad7612ea700b9a0e3733148
LeeHZ <asd56780@naver.com>
2023-12-08 02:15:53 +0900

    add : 크롤링 로직 추가

---
+++ backend/src/main/java/com/doge/backend/data/crawling/SeleniumController.java
@@ -0,0 +1,166 @@
+package hello.hellospring.crawling;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.*;
+
+@RestController
+public class SeleniumController {
+    private int pageNum = 1;
+    private int start = 1;
+    private int loop = 200;
+    private final int add = 200;
+    private final int end = 16292;
+    private final int cpp = 50;
+    private final int MAX_RETRY = 3;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private long printTimestamp() {  // 타임스탬프 출력
+        long currentTime = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(currentTime);
+        SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");    // 해당 format으로 출력
+        System.out.println(sdf.format(timestamp));
+
+        return currentTime;
+    }
+
+    private void printInsertSQL(StringBuilder result) {  // 크롤링한 데이터를 Insert문으로 정리하여 txt파일로 저장
+        BufferedOutputStream bs = null;
+        try{
+            bs = new BufferedOutputStream(new FileOutputStream("C:\\IntelliJ_Workspace\\hello-spring\\src\\main\\resources\\sql_crawl\\"+ pageNum +".txt"));
+            bs.write(result.toString().getBytes());
+        } catch (Exception e){
+            e.getStackTrace();
+            System.err.println("Exception 발생: 파일 입출력 에러");
+        } finally {
+            if (bs != null){
+                try {
+                    bs.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.err.println("IOException 발생: 파일 입출력 null 에러");
+                }
+            }
+        }
+    }
+
+    private void sqlExecute(StringBuilder result) {  // Insert문 db에 실행
+        String sql = result.toString();
+        try {
+            jdbcTemplate.execute(sql);
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void clearTaskkill() throws IOException {   // chromedriver가 정상적으로 종료되지 않고 누적되는 것을 막기 위해 한번의 루프가 끝날 때마다 taskkill을 입력
+        Runtime.getRuntime().exec("cmd /c " + "taskkill /f /im chromedriver.exe");
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException ie){
+            System.err.println("InterruptedException 발생: taskkill 이후 timeout 에러");
+            ie.printStackTrace();
+        }
+    }
+
+    private String URLgenerator(int page){
+        String pageUrl = "https://lib.dongguk.edu/search/tot/result?pn=" + Integer.toString(page) +
+                "&st=FRNT&commandType=advanced&mId=&si=12&q=0&b0=or&weight0=&si=12&q=1&b1=or&weight1=&si=12&q=2&b2=or&weight2=&si=12&q=3&b3=or&weight3=&si=12&q=4&b4=or&weight4=&si=12&q=5&b5=or&weight5=&si=12&q=6&b6=or&weight6=&si=12&q=7&b7=or&weight7=&si=12&q=8&b8=or&weight8=&si=12&q=9&weight9=&_lmt0=on&lmtsn=000000000001&lmtst=OR&lmt0=m&_lmt0=on&_lmt0=on&_lmt0=on&_lmt0=on&_lmt0=on&_lmt0=on&_lmt0=on&_lmt0=on&_lmt0=on&inc=TOTAL&_inc=on&_inc=on&_inc=on&_inc=on&_inc=on&lmt1=TOTAL&lmtsn=000000000003&lmtst=OR&lmt2=0000000A&lmtsn=000000000006&lmtst=OR&rf=&rt=&range=000000000021&" +
+                "cpp=" + Integer.toString(cpp) + "&msc=814354";
+        return pageUrl;
+    }
+
+    private StringBuilder processFutures(List<Future<String>> futures) {
+        int count = 0;
+        int retry_flag = 0;
+        StringBuilder result = new StringBuilder();
+        for (Future<String> future : futures) {
+            count++;
+            while (retry_flag < MAX_RETRY){
+                try {
+                    // 각 스레드의 작업 결과를 받아옴
+                    result.append(future.get());
+                    break;  // 성공적으로 결과를 받았으면 반복문 종료
+                } catch (InterruptedException | ExecutionException e) {
+                    System.err.println("InterruptedException or ExecutionException 발생: StringBuilder 에러 num: " + pageNum + " - " + count + " - " + retry_flag);
+                    e.printStackTrace();
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(1000);
+                    } catch (InterruptedException ie){
+                        System.err.println("InterruptedException 발생: StringBuilder 이후 timeout 에러");
+                        ie.printStackTrace();
+                    } finally {
+                        retry_flag++;
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                    retry_flag++;
+                }
+            }
+        }
+        return result;
+    }
+
+    @GetMapping("crawler")
+    public String selenium() throws IOException {
+        // 타임스탬프 출력
+        System.out.println("####START####");
+        long beforeTime = printTimestamp();
+
+        while(loop <= end){
+            // Thread pool 개수 설정
+            ExecutorService executor = Executors.newFixedThreadPool(10);
+            List<Future<String>> futures = new ArrayList<>();
+
+            for (int i = start; i <= loop; i++){
+                String pageUrl = URLgenerator(i);
+                Future<String> future = executor.submit(new SeleniumCrawler(pageUrl, i));
+                futures.add(future);
+            }
+
+            // 크롤링 결과를 받아와서 StringBuilder로 만듦
+            StringBuilder result = processFutures(futures);
+
+            executor.shutdown();
+            try {
+                executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            } catch (InterruptedException e) {
+                System.err.println("InterruptedException 발생: Thread 종료 에러");
+                e.printStackTrace();
+            }
+
+            // SQL 출력 및 실행
+            printInsertSQL(result);
+            sqlExecute(result);
+
+            // 루프마다 chromedriver 정리
+            clearTaskkill();
+
+            pageNum += 1;
+            start += add;
+            loop += add;
+        }
+
+        // 타임스탬프 출력
+        System.out.println("####END####");
+        long afterTime = printTimestamp();
+        System.out.print((afterTime - beforeTime)/60000 + "min ");
+        System.out.println((afterTime - beforeTime)%60000/1000 + "sec");
+
+        return "Data 크롤링 완료";
+    }
+}
---
+++ backend/src/main/java/com/doge/backend/data/crawling/SeleniumCrawler.java
@@ -0,0 +1,238 @@
+package hello.hellospring.crawling;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+public class SeleniumCrawler implements Callable<String> {
+    private final int cpp = 50;
+    private final int MAX_RETRY = 3;
+    private String url;
+    private int loop;
+    private Path path = Paths.get("C:\\webdriver\\chromedriver-win64\\chromedriver.exe");
+    private WebDriver driver;
+    private WebDriverWait webDriverWait;
+    private final int WEB_DRIVER_WAIT_TIME = 20;
+
+    public SeleniumCrawler(String url, int loop) {
+        this.url = url;
+        this.loop = loop;
+    }
+
+    public ChromeOptions createChromeOptions() {    // Webdriver 세팅
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-popup-blocking");   // 팝업 안띄움
+        options.addArguments("--headless");   // 브라우저 안띄움
+        options.addArguments("--disable-gpu");  // gpu 비활성화
+        options.addArguments("--lang=ko");
+        options.addArguments("--blink-settings=imagesEnabled=false");   // 이미지 다운 안받음
+        options.addArguments("--disable-ipc-flooding-protection");
+        options.addArguments("--disable-extensions");   // 확장 프로그램 비활성화
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-logging");
+
+        Map<String, Object> contentSettings = getStringObjectMap();
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("profile.default_content_setting_values", contentSettings);
+
+        options.setExperimentalOption("prefs", prefs);
+
+        return options;
+    }
+
+    private static Map<String, Object> getStringObjectMap() {   // Webdriver 옵션
+        Map<String, Object> contentSettings = new HashMap<>();
+        contentSettings.put("cookies", 2);
+        contentSettings.put("images", 2);
+        contentSettings.put("plugins", 2);
+        contentSettings.put("popups", 2);
+        contentSettings.put("geolocation", 2);
+        contentSettings.put("notifications", 2);
+        contentSettings.put("auto_select_certificate", 2);
+        contentSettings.put("fullscreen", 2);
+        contentSettings.put("mouselock", 2);
+        contentSettings.put("mixed_script", 2);
+        contentSettings.put("media_stream", 2);
+        contentSettings.put("media_stream_mic", 2);
+        contentSettings.put("media_stream_camera", 2);
+        contentSettings.put("protocol_handlers", 2);
+        contentSettings.put("ppapi_broker", 2);
+        contentSettings.put("automatic_downloads", 2);
+        contentSettings.put("midi_sysex", 2);
+        contentSettings.put("push_messaging", 2);
+        contentSettings.put("ssl_cert_decisions", 2);
+        contentSettings.put("metro_switch_to_desktop", 2);
+        contentSettings.put("protected_media_identifier", 2);
+        contentSettings.put("app_banner", 2);
+        contentSettings.put("site_engagement", 2);
+        contentSettings.put("durable_storage", 2);
+        return contentSettings;
+    }
+
+    private void initializeWebDriver() {    // Webdriver 초기화
+        System.setProperty("webdriver.chrome.driver", path.toString());
+
+        int sysRetryFlag = 0;
+        while(sysRetryFlag < MAX_RETRY) {
+            try {
+                ChromeOptions options = createChromeOptions();
+                this.driver = new ChromeDriver(options);
+
+                this.webDriverWait = new WebDriverWait(this.driver, Duration.ofSeconds(WEB_DRIVER_WAIT_TIME));    // 드라이버가 실행된 후 20초 기다림
+                break;
+            } catch (SessionNotCreatedException se) {
+                System.err.println("SessionNotCreatedException 발생 - 에러: Driver Config");
+                se.printStackTrace();
+                sysRetryFlag++;
+                try {
+                    this.driver.quit();
+                } catch (NullPointerException ne) {
+                    System.err.println("NullPointerException 발생 - 에러: driver가 생성되지 않음");
+                    ne.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private String processData() {
+        String result = "INSERT INTO BOOK (CALL_NUMBER, BOOK_NAME, AUTHOR, PUBLISHER, PHOTO_LINK) VALUES ";
+
+        List<WebElement> contents = getContent();
+
+        javaScriptExecute(contents);
+
+        result += extractDataFromElement(contents);
+
+        return result;
+    }
+
+    private List<WebElement> getContent() { // WebElement를 긁어옴
+        By elementLocator = By.cssSelector("#divContent > div > div.briefContent > div.result > form > fieldset > ul > li");
+        this.webDriverWait.until(ExpectedConditions.presenceOfElementLocated(elementLocator));
+        return this.driver.findElements(elementLocator);
+    }
+
+    private void javaScriptExecute(List<WebElement> contents) { // 도서 청구기호 크롤링을 위해 접힌 부분 클릭
+        JavascriptExecutor js = (JavascriptExecutor) this.driver;
+        int bookCount = 0;
+
+        for (WebElement holding : contents) {
+            WebElement hold = holding.findElement(By.cssSelector("dl > dd.holdingInfo > div > p > a"));
+            js.executeScript("arguments[0].click()", hold);
+            System.out.println(bookCount++);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    private String extractDataFromElement(List<WebElement> contents) {
+        String result = "";
+        int bookCount = 0, error_code;
+        String callNum, title, author, publisher;
+        String callNumSelector = "dl > dd.holdingInfo > div.holding > div.holdingW > div.listTable > table > tbody > tr > td.callNum";
+        String titleSelector = "dl > dd.title > a";
+        String authorSelector = "dl > dd:nth-child(10)";
+        String publisherSelector = "dl > dd:nth-child(12)";
+        String imageSelector = "dl > dd.book > a > img";
+
+        if (!contents.isEmpty()){
+            for (WebElement content : contents) {
+
+                // 도서 청구기호 추출
+                try {
+                    callNum = content.findElement(By.cssSelector(callNumSelector)).getAttribute("textContent").replace("'", "''");
+                    result += "('" + callNum + "',";
+                } catch (NoSuchElementException e) {
+                    error_code = ((loop-1)*cpp)+(bookCount+1);
+                    System.out.println("Error : " + error_code);
+                    e.printStackTrace();
+                    continue;
+                }
+
+                // 도서 제목 추출
+                title = content.findElement(By.cssSelector(titleSelector)).getText().replace("'", "''");
+                result += "'" + title + "',";
+
+                // 도서 저자 추출
+                try {
+                    author = content.findElement(By.cssSelector(authorSelector)).getText().replace("'", "''");
+                    result += "'" + author + "',";
+                } catch (NoSuchElementException e) {
+                    result += NoSuchElementExceptionHandler(bookCount, "author");
+                }
+
+                // 도서 출판사 추출
+                try {
+                    publisher = content.findElement(By.cssSelector(publisherSelector)).getText().replace("'", "''");
+                    result += "'" + publisher + "',";
+                } catch (NoSuchElementException e) {
+                    result += NoSuchElementExceptionHandler(bookCount, "publisher");
+                }
+
+                // 도서 이미지 링크 추출
+                WebElement img = content.findElement(By.cssSelector(imageSelector));
+                String src = img.getAttribute("src").replace("'", "''");
+                result += "'" + src + "')";
+
+                bookCount++;
+
+                // 마지막 책이라면 ;으로 sql문 닫기
+                if (bookCount == cpp) {
+                    result += "; ";
+                    break;
+                }
+                else {
+                    result += ",\n";
+                }
+            }
+        }
+        return result;
+    }
+
+    private String NoSuchElementExceptionHandler(int count, String exceptionPlace) {
+        int error_code = ((loop-1)*cpp)+(count+1);
+        System.out.println(error_code + " : " + exceptionPlace + " replaced NULL");
+        return " NULL ,";
+    }
+
+    @Override
+    public String call() throws Exception {
+        this.driver = null;
+        String result = "";
+        initializeWebDriver();
+
+        int retryFlag = 0;
+        while(retryFlag < MAX_RETRY) {
+            try {
+                this.driver.get(url);
+                result += processData();
+                break;
+            } catch (SessionNotCreatedException se) {
+                System.err.println("SessionNotCreatedException 발생 - 에러: " + url);
+                retryFlag++;
+            } catch (TimeoutException te) {
+                System.err.println("TimeoutException 발생 - 에러: " + url);
+                retryFlag++;
+            } finally {
+                this.driver.quit();
+            }
+        }
+
+        // 5000권마다 진행 척도 콘솔에 출력
+        if (loop%100 == 0) System.out.println(loop*50);
+        return result;
+    }
+}
